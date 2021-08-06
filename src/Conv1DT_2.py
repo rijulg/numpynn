438f688f43f74a8c024d2cdecaba47000046a092
Rijul Gupta <rijulg@gmail.com>
2021-08-06 13:34:28 +0000

    replaced matmul with dot

--- src/Conv1DT_2.py
+++ src/Conv1DT_2.py
@@ -8,12 +8,13 @@ class Conv1DT:
         inchan, outchan, kernel_len = weight.shape
         outlen = int((inlen-1)*stride - 2*padding + kernel_len)
         x = x.T # [inlen, inchan]
-        w = weight.transpose(1,0,2) # [outchan, inchan, kernel]
-        temp = x @ w # [outchan, inlen, kernel]
+        w = weight.transpose(1, 0, 2) # [outchan, inchan, kernel]
+        temp = np.dot(x, w) # [outchan, inlen, kernel]
+        print(temp.shape)
         y = np.zeros((outchan, outlen + 2*padding))
         for i in range(inlen):
             yi = stride*i
-            y[:, yi:yi+kernel_len] += temp[:, i, :]
+            y[:, yi:yi+kernel_len] += temp[i]
         y += bias[:, None]
         if padding > 0:
             return y[:, padding:-padding]
